f5f28bd5d28ca06dad6d2a442a0af1dda55be3b2
Kyle Harrer <harrer.kyle@gmail.com>
2018-05-22 17:21:59 -0400

    handlebars stuff

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,32 @@
 const expr = require('express');
 const burger = require('../models/burger');
+const router = expr.Router();
 
+router.get('/', function(req,res) {
+	res.redirect('/burger')
+});
+
+router.get('/burger', function(req,res) {
+
+	Burger.all(data => {
+		let hbsObject = {burgers : data}
+		console.log(hbsObject)
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/burger/create', (req,res) => {
+	burger.create(['name', 'devoured'], [req.body.name, req.body.devoured], (data) => {
+		res.redirect('/burger')
+	});
+});
+
+router.put('/burger/update/:id', (req,res) => {
+	let condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	burger.update({'devoured' : req.body.devoured}, condition, (data) => {
+		res.redirect('/burger');
+	});
+});
